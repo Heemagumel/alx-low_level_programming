3e9438d62844704640e809c65adc29dc1e5daad7
heemagumel <ibraheemaliyuuthman@gmail.com>
2023-02-22 15:01:56 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
-/** main - main function
-* _putchar: print the alphabets putchar
-*Return: always 0
-*/
+/**main - main function
+ * _putchar: print alphabet putchar
+ * Return: always 0
+ */
 
 int main(void)
 {
-_putchar(_);
-_putchar(p);
-_putchar(u);
-_putchar(t);
-_putchar(c);
-_putchar(h);
-_putchar(a);
-_putchar(r);
-_putchar(n);
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('h');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
