ec1e54c03c436b53821a0efadd690c9a0d768395
heemagumel <ibraheemaliyuuthman@gmail.com>
2023-02-23 17:45:58 -0800

    largest num corr

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,7 +14,7 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
-		largest = a;
+	largest = a;
 	}
 	else if (a > b && c > a)
 	{
@@ -22,12 +22,12 @@ int largest_number(int a, int b, int c)
 	}
 	else if (b > c)
 	{
-		largest = b;
+	largest = b;
 	}
 	else
 	{
-		largest = c;
+	largest = c;
 	}
 
-	return (largest);
+return (largest);
 }
