eec9c0904576b5c7c693d18691dbe61afeb2d503
heemagumel <ibraheemaliyuuthman@gmail.com>
2023-03-07 18:01:59 -0800

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,16 +1,15 @@
 #include "main.h"
-
 /**
- * _strspn - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
- */
-
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
 	int r;
+
 	while (*s)
 	{
 		for (r = 0; accept[r]; r++)
@@ -21,7 +20,7 @@ unsigned int _strspn(char *s, char *accept)
 				break;
 			}
 			else if (accept[r + 1] == '\0')
-				return (n)
+				return (n);
 		}
 		s++;
 	}
